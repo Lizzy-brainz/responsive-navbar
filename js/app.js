3ba003214e3be739660491e75b82a8eeecf588f4
Lizzy-brainz <onojalizzy9@gmail.com>
2021-03-25 15:17:29 -0700

    navbar is now responsive

--- js/app.js
+++ js/app.js
@@ -1,24 +1,10 @@
-const navSlide = ()=>{
-    const nav_toggle = document.querySelector('.nav_toggle');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-   
-    nav_toggle.addEventListener('click', ()=> {
-        // TOGGLE NAVBAR
-        nav.classList.toggle('nav-active');
-         // ANIMATE LINKS
-    navLinks.forEach((link, index)=>{
-        if(link.style.animation){
-            link.style.animation = ''
-        }
-        else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-        }
-            });
-            // Toggle Animation
-            nav_toggle.classList.toggle('toggle');
-    });
-   
-}
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementById('navbar-links')
+const toggleBars = document.querySelectorAll('.toggle-button .bar')
 
-navSlide();
+toggleButton.addEventListener('click', ()=>{
+    navbarLinks.classList.toggle('block')
+    toggleBars.forEach((bar) => {
+        bar.classList.toggle('active');
+    })
+})
